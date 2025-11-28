65bbc9cb0bc8c56a5a3530d76a6bcbe1d7bfd0f5
mariana <eumarianamota@gmail.com>
2025-11-28 12:34:12 -0300

    feat: create continents page and redirect button

--- js/script.js
+++ js/script.js
@@ -9,3 +9,40 @@ const handleClick = () => {
 }
 
 mobileMenu.addEventListener("click", handleClick) 
+
+const buttons = document.querySelectorAll(".btn-redirect-page")
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const textButton = button.textContent.trim()
+        let url = ""
+
+        switch (textButton) {
+            case 'Explorar América':
+                url = '/html/america.html'
+                break
+            case 'Explorar África': 
+                url = '/html/africa.html'
+                break
+            case 'Explorar Antártida':
+                url = '/html/antartida.html'
+                break
+            case 'Explorar Ásia':
+                url = '/html/asia.html'
+                break
+            case 'Explorar Europa':
+                url = '/html/europa.html'
+                break
+            case 'Explorar Oceania': 
+                url = '/html/oceania.html'
+                break
+        }
+
+        console.log('Valor final da variável url:', url)
+
+        if(url) {
+            console.log('Deu certo')
+            window.location.href = url
+        }
+    })
+})
