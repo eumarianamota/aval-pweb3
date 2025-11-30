8489019d2dff9e5539fce9a81a3b0345b843299b
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-11-30 14:18:05 -0300

    feat(js): implement country details display with spinner and error handling

--- js/country.js
+++ js/country.js
@@ -5,3 +5,34 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const params = new URLSearchParams(window.location.search);
     const countryName = params.get("name");
+
+    if (!countryName) {
+        detailsContainer.innerHTML = "<p>País não informado.</p>";
+        return;
+    }
+    try{
+        spinner.style.display = "block";
+        const response = await fetch(`${API_URL}/name/${countryName}`);
+        if (!response.ok) throw new Error("País não encontrado");
+        const country = (await response.json())[0];
+
+        const languages = country.languages ? Object.values(country.languages).join(", ") : "N/A";
+        const currecies = country.currencies ? Object.values(country.currencies).map(c => c.name).join(", ") : "N/A";
+        const borders = country.borders ? country.borders.join(", ") : "N/A";
+
+        detailsContainer.innerHTML = `
+            <h2>${country.name.common}</h2>
+            <img src="${country.flags.svg}" alt="Bandeira de ${country.name.common}" width="200"/>
+            <p><strong>Capital:</strong> ${country.capital ? country.capital[0] : "N/A"}</p>
+            <p><strong>Região:</strong> ${country.region}</p>
+            <p><strong>População:</strong> ${country.population.toLocaleString()}</p>
+            <p><strong>Línguas:</strong> ${languages}</p>
+            <p><strong>Moedas:</strong> ${currecies}</p>
+            <p><strong>Fronteiras:</strong> ${borders}</p>
+        `;
+    } catch (error) {
+        detailsContainer.innerHTML = `<p>Erro ao carregar os detalhes do país: ${error.message}</p>`;
+    } finally {
+        spinner.style.display = "none";
+    }
+});
